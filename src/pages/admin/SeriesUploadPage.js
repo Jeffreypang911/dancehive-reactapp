18f1b11f6957abed9f14c10a5bec6b92f9fea6ef
Mel <mchuam@gmail.com>
2018-11-27 09:55:02 -0800

    added handlers for form elements, so now it saves to state. next up is to get this sending to cloudinary and firestore database

--- src/pages/admin/SeriesUploadPage.js
+++ src/pages/admin/SeriesUploadPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import '../../firebase/firebase';
-
 
 class SeriesUploadPage extends Component {
     state = {
@@ -11,10 +10,23 @@ class SeriesUploadPage extends Component {
         description: '',
     }
 
+    handleChange = (e) => {
+        const { target } = e;
+        const { name } = target;
+        const { value } = target;
+        console.log(value);
+        console.log(name);
+        this.setState({
+            [name]: value // need to ask about this syntax
+        });
+    }
+
     onSubmit = (e) => {
         e.preventDefault();
+        // console.log(e);
         console.log('submitting');
-        console.log('series name', this.state.seriesName);
+        console.log('series name: ', this.state.seriesName);
+        console.log('instructor name: ', this.state.instructorName);
     }
     render() {
         return (
@@ -23,19 +35,46 @@ class SeriesUploadPage extends Component {
                 <Form className="form" onSubmit={ (e) => this.onSubmit(e)}>
                     <FormGroup>
                         <Label for="seriesName">Series Name</Label>
-                        <Input name="seriesName" placeholder="ie. Sensual Fundamentals" />                        
+                        <Input 
+                            name="seriesName" 
+                            placeholder="ie. Sensual Fundamentals"
+                            // value={ seriesName }
+                            onChange={ (e) => {
+                                this.handleChange(e);
+                            }}
+                        />                        
                     </FormGroup>
                     <FormGroup>
                         <Label for="instructorName">Instructor</Label>
-                        <Input name="instructorName" placeholder="ie. Joe Boxer"/>
+                        <Input 
+                            name="instructorName" 
+                            placeholder="ie. Joe Boxer"
+                            onChange={ (e) => {
+                                this.handleChange(e);
+                            }}
+                        />
                     </FormGroup>
                     <FormGroup>
                         <Label for="seriesPoster">Poster URL</Label>
-                        <Input type="url" name="seriesPoster" placeholder="ie. http://www.image.com/image.jpg " />
+                        <Input 
+                            type="url" 
+                            name="seriesPoster" 
+                            placeholder="ie. http://www.image.com/image.jpg"
+                            onChange={ (e) => {
+                                this.handleChange(e);
+                            }} 
+                        />
                     </FormGroup>
                     <FormGroup>
                         <Label for="description">Description</Label>
-                        <Input type="textarea" name="description" placeholder="Description here" />                        
+                        <Input 
+                            type="textarea" 
+                            name="description" 
+                            placeholder="Description here"
+                            onChange={ (e) => {
+                                this.handleChange(e);
+                            }} 
+                        />                        
                     </FormGroup>
                     <Button color="primary">Submit</Button>
                 </Form>
